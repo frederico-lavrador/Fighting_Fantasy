d2bbf59d90cf96875417ab40bce21861b9a45da2
frederico-lavrador <frederico.lavrador@gmail.com>
2024-10-25 12:55:21 +0100

    feat: created stat table

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,43 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <p>App</p>
-    </>
-  )
+		<>
+			<div className='container'>
+				<div className='app__wrapper'>
+					<div className='header'>
+						<div className='title'>
+							<h1>Fighting Fantasy Adventure Manager</h1>
+						</div>
+					</div>
+					<div className='app__content'>
+						<div className='stats__table'>
+							<h2>Stats</h2>
+							<div className='stats__stats'>
+								<div className='stats__name'>
+									<h4>Name</h4>
+									<p>Skill</p>
+									<p>Stamina</p>
+									<p>Luck</p>
+								</div>
+								<div className='stats__initial'>
+									<h4>Initial</h4>
+									<p>0</p>
+									<p>0</p>
+									<p>0</p>
+								</div>
+								<div className='stats__current'>
+									<h4>Current</h4>
+									<p>0</p>
+									<p>0</p>
+									<p>0</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App
